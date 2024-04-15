a4d6970a38dc0b70ba75571faa51e305f61916ea
Alvin <70914856+pishori@users.noreply.github.com>
2024-04-15 19:43:24 +0300

    Update twitterButton.jsx

--- src/components/twitterButton.jsx
+++ src/components/twitterButton.jsx
@@ -4,8 +4,8 @@ import { FaTwitter } from 'react-icons/fa';
 const TwitterButton = (props) => (
     <a
     id='tweet-quote'
-    href={}
-    style={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`{
+    href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`}
+    style={
         marginRight: "10px",
         backgroundColor: props.style
     }}
